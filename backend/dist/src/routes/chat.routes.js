39b989ac8fdc0c8d0943f7aba59ce62161180aad
Raghav-73 <coder6625raghav@gmail.com>
2026-02-14 18:43:02 +0530

    fixed hono error

--- backend/dist/src/routes/chat.routes.js
+++
@@ -1,8 +0,0 @@
-import { Hono } from 'hono';
-import { sendMessage, getConversation, listConversations, deleteConversation } from '../controllers/chat.controller.js';
-const chat = new Hono();
-chat.post('/', sendMessage);
-chat.get('/conversations/:id', getConversation);
-chat.get('/conversations', listConversations);
-chat.delete('/conversations/:id', deleteConversation);
-export default chat;
