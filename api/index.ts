a713903e31c7220bc907e3b4fba988ce4c7a68a0
Raghav-73 <coder6625raghav@gmail.com>
2026-02-14 14:52:54 +0530

    Uploading prod to vercel

--- api/index.ts
+++
@@ -1,8 +0,0 @@
-import { handle } from 'hono/vercel'
-import app from '../backend/dist/src/index.js'
-
-export const config = {
-    runtime: 'nodejs18.x'
-}
-
-export default handle(app)
--- backend/api/index.ts
+++ backend/api/index.ts
@@ -2,7 +2,7 @@ import { handle } from 'hono/vercel'
 import app from '../dist/src/index.js'
 
 export const config = {
-    runtime: 'nodejs18.x'
+    runtime: 'nodejs'
 }
 
 export default handle(app)
